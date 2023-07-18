86331358e249b6f4e92de9ca9d5617c6eba0c220
Adeola <aadelakun28@gmail.com>
2023-07-18 16:22:37 +0100

    Update batch-transfer.js

--- scripts/batch-transfer.js
+++ scripts/batch-transfer.js
@@ -62,39 +62,6 @@ main()
 
 
 
-// const { ethers } = require("hardhat");
-// const contractInJSON = require("../artifacts/contracts/AdesdeskNFTCollection.sol/AdesdeskNFTCollection.json");
-// require("dotenv").config();
-
-// async function main() {
-//   // Set up the signer with your MetaMask private key
-//   const privateKey = process.env.PRIVATE_KEY;
-//   const AdesdeskNFTCollectionABI = contractInJSON.abi;
-//   const AdesdeskNFTCollectionAddress = '0x5B30a9CcE60FB6c8099c643e018Ed278ed9be6F7';
-//   if (!privateKey) {
-//     throw new Error("Private key not found in the .env file.");
-//   }
-
-//   // Get the wallet provider for the Goerli network
-//   const provider = new ethers.providers.JsonRpcProvider(process.env.GOERLI_URL);
-//   const signer = new ethers.Wallet(privateKey, provider);
-//   AdesdeskNFTCollectionContract = new ethers.Contract(AdesdeskNFTCollectionAddress, AdesdeskNFTCollectionABI, provider);;
-
-
-
-
-//   console.log("Transfers completed!");
-// }
-
-// // Execute the batch minting script
-// main()
-//   .then(() => process.exit(0))
-//   .catch((error) => {
-//     console.error(error);
-//     process.exit(1);
-//   });
-
-
 
 
 
